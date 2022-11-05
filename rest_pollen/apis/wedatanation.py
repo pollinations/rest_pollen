1c87428b168c1397215047ed8a4c0524a610ad32
Niels Warncke <niels.warncke@gmail.com>
2022-11-05 16:40:34 +0100

    linting

--- rest_pollen/apis/wedatanation.py
+++ rest_pollen/apis/wedatanation.py
@@ -1,20 +1,15 @@
-import click
-import uvicorn
-from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from pypollsdk.model import run_model
 
 from rest_pollen.authentication import TokenPayload, get_current_user
 
-from pypollsdk.model import run_model
-
-
-load_dotenv()
-
 app = FastAPI()
 
-AVATAR_IMAGE = "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/wedatanation-pick-avatar"
+AVATAR_IMAGE = (
+    "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/wedatanation-pick-avatar"
+)
 INDEX_CID = ""
 
 
@@ -56,10 +51,9 @@ async def root():
 async def generate(
     avatar_request: AvatarRequest, user: TokenPayload = Depends(get_current_user)
 ) -> AvatarResponse:
-    response = run_model(AVATAR_IMAGE, {
-        "index_cid": INDEX_CID,
-        "prompt": avatar_request.description
-    })
+    response = run_model(
+        AVATAR_IMAGE, {"index_cid": INDEX_CID, "prompt": avatar_request.description}
+    )
     pollen_response = AvatarResponse(
         input=avatar_request.input,
         output=response["output"],
--- test/client.py
+++ test/client.py
@@ -3,9 +3,7 @@ from utils import generate_test_token
 
 request = {
     "image": "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private",
-    "input": {
-        "prompts": "A knight on a horse made out of stars and a galactic nebula"
-    }
+    "input": {"prompts": "A knight on a horse made out of stars and a galactic nebula"},
 }
 
 

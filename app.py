a5c04f97758e1a9dd0d7ec2662b69ebe593e1b8a
Niels Warncke <niels.warncke@gmail.com>
2022-11-09 15:35:47 +0100

    add dev cdk stack

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
+import os
+
 import aws_cdk as cdk
 
 from cdk.cdk_stack import CdkStack
 
+dev_or_prod = os.environ.get("DEV_OR_PROD", "dev")
+
+if dev_or_prod == "dev":
+    stack_name = "pollen-rest-api-dev"
+else:
+    stack_name = "pollen-rest-api"
+
 app = cdk.App()
 CdkStack(
     app,
-    "pollen-rest-api",
+    stack_name,
     env=cdk.Environment(account="614871946825", region="us-east-1"),
 )
 

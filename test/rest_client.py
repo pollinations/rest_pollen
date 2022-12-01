72f67c12ee56065b98c164b300cf2560bcacccb8
Niels Warncke <niels.warncke@gmail.com>
2022-12-01 16:11:02 +0100

    added manual tests for local, dev and prod in addition to unit tests

--- test/rest_client.py
+++ test/rest_client.py
@@ -1,3 +1,4 @@
+import click
 import requests
 from utils import generate_test_token  # noqa F401
 from utils import get_dreamachine_request  # noqa F401
@@ -5,6 +6,7 @@ from utils import get_lemonade_request  # noqa F401
 from utils import get_replicate_stablediffusion_request  # noqa F401
 from utils import get_stablediffusion_request  # noqa F401
 from utils import get_wedatanation_request  # noqa F401
+from ws_client import WebsockerClosed, ws_client
 
 backend_url = "https://rest.pollinations.ai"
 backend_url = "http://localhost:5000"
@@ -41,7 +43,34 @@ def client(request):
     print(response)
 
 
+backends = {
+    "prod": "https://rest.pollinations.ai",
+    "dev": "https://worker-dev.pollinations.ai",
+    "local": "http://localhost:5000",
+}
+backend_url = None
+
+
+@click.command()
+@click.argument("backend")
+def main(backend):
+    global backend_url
+    backend_url = backends[backend]
+    try:
+        ws_client(backend, get_lemonade_request())
+    except WebsockerClosed:
+        pass
+    # cached, lemonade
+    client(get_lemonade_request())
+    # cached, replicate backend
+    client(get_dreamachine_request())
+    # cached, pollinations backend
+    client(get_stablediffusion_request())
+    # uncached, replicate backend
+    client(get_replicate_stablediffusion_request(True))
+    # uncached, pollinations backend
+    client(get_stablediffusion_request(True))
+
+
 if __name__ == "__main__":
-    # wedatanation_client()
-    # lemonade_client()
-    client(get_replicate_stablediffusion_request())
+    main()
